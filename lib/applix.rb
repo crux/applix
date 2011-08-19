42d28f5c318f62d3ddb8e4951ec8fba25ca5e065
dirk luesebrink <dirk.luesebrink@gmail.com>
2011-08-19 16:39:44 +0200

    code clean

--- lib/applix.rb
+++ lib/applix.rb
@@ -83,38 +83,3 @@ usage: #{$0} <args...>
     @tasks ||= {}
   end
 end
-
-__END__
-#
-def main args, options = {}
-  options = (Defaults.merge options)
-  options[:date] = Date.parse(options[:date]) # up-type string date
-
-  action = args.shift or raise "no action"
-
-  # account is an command line arg but password is prompted, never have that in
-  # a config or on the command line!
-  #
-  username = args.shift # or raise "no username"
-  password = prompt_for_password
-
-  # which method to run depend on first command line argument..
-  send action, username, password, options
-end
-
-params = Hash.from_argv ARGV
-begin 
-  main params[:args], params
-rescue => e
-  puts <<-EOT
-
-## #{e}
-
-usage: #{__FILE__} <task> <username>
-
---- #{e.backtrace.join "\n    "}
-  EOT
-end
-
-__END__
-
