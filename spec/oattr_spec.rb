00e52bf89ec20d3b3e9996219fdfe4a4e5bcbcb7
dirk lüsebrink <dirk.luesebrink@gmail.com>
2012-03-27 01:20:20 +0200

    minor clean-up

--- spec/oattr_spec.rb
+++ spec/oattr_spec.rb
@@ -1,6 +1,6 @@
-require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+require 'spec_helper'
 
-describe "OAttr" do
+describe OAttr do
   it "should define a module" do
     OAttr.should_not == nil
   end
